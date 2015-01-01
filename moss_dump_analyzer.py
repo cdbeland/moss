425806ed0899f2e640fc2880a489b73aa53f5204
Christopher Beland <beland@alum.mit.edu>
2015-01-01 03:35:43 -0500

    PEP8 for moss_dump_analyzer.py

--- moss_dump_analyzer.py
+++ moss_dump_analyzer.py
@@ -37,30 +37,31 @@ import re
 # Ukrainian alphabet | &amp;amp;#8222; &amp;amp;#8223;
 # Quotation marks | U+201F || {{smallcaps|{{lc:Double high-reversed-9 quotation mark}}}} || &amp;amp;#8223;
 
+
 def read_en_article_text(callback_function):
     working_string = ""
-    
+
     with open("/bulk-wikipedia/enwiki-20141208-pages-articles-multistream.xml", "r") as article_xml_file:
-         for line in article_xml_file:
-              working_string += line
-              if line == "  </page>\n":
-                   working_string = re.sub(r"^.*(<page.*?</page>).*$", r"\1", working_string, flags=re.MULTILINE+re.DOTALL)
-                   root_element = lxml.etree.fromstring(working_string)
-                   working_string = ""
+        for line in article_xml_file:
+            working_string += line
+            if line == "  </page>\n":
+                working_string = re.sub(r"^.*(<page.*?</page>).*$", r"\1", working_string, flags=re.MULTILINE+re.DOTALL)
+                root_element = lxml.etree.fromstring(working_string)
+                working_string = ""
 
-                   namespace = root_element.findtext('ns')
-                   if namespace != '0':
-                        # Page is not an article
-                        continue
+                namespace = root_element.findtext('ns')
+                if namespace != '0':
+                    # Page is not an article
+                    continue
 
-                   redirect_target = root_element.find('redirect')
-                   if redirect_target is not None:
-                        # Article is redirect; skip it for now
-                        continue
+                redirect_target = root_element.find('redirect')
+                if redirect_target is not None:
+                    # Article is redirect; skip it for now
+                    continue
 
-                   article_title = root_element.findtext('title')
-                   article_text = root_element.findtext('.//text')                   
-                   callback_function(article_title, article_text)
+                article_title = root_element.findtext('title')
+                article_text = root_element.findtext('.//text')
+                callback_function(article_title, article_text)
 
 
 def changer_callback(article_text, article_title):
@@ -75,60 +76,59 @@ def changer_callback(article_text, article_title):
 
 
 def make_changes(input_wikitext):
-     segments = segmentize(input_wikitext)
-     output_wikitext = ''
-     edit_summaries = []
-     change_count = 0
+    segments = segmentize(input_wikitext)
+    output_wikitext = ''
+    edit_summaries = []
+    change_count = 0
 
-     # TODO: English dialect detector
-     dialect = 'unknown'
+    # TODO: English dialect detector
+    dialect = 'unknown'
 
-     for segment in segments:
-          wikitext = segment['wikitext']
-          if segment['type'] == 'normal':
+    for segment in segments:
+        wikitext = segment['wikitext']
+        if segment['type'] == 'normal':
 
-               if dialect == 'american':
-                    # American and Canadian English = U.S.
-                    new_wikitext = re.sub("USA", "U.S.", wikitext)
-                    if new_wikitext != wikitext:
-                         change_count += 1
-                         wikitext = new_wikitext
-                         edit_summaries.append("USA -> U.S. per [[WP:NOTUSA]]")
-               elif dialect == 'british':
-                    # British/Commonwealth English = US
-                    pass
-
-
-               new_wikitext = re.sub(u"[“”]", r'"', wikitext)
-               # U+201C, U+201D
-               if new_wikitext != wikitext:
+            if dialect == 'american':
+                # American and Canadian English = U.S.
+                new_wikitext = re.sub("USA", "U.S.", wikitext)
+                if new_wikitext != wikitext:
                     change_count += 1
-                    edit_summaries.append('“” -> " per [[Wikipedia:Manual of Style#Quotation_marks]]')
                     wikitext = new_wikitext
+                    edit_summaries.append("USA -> U.S. per [[WP:NOTUSA]]")
+            elif dialect == 'british':
+                # British/Commonwealth English = US
+                pass
 
-               """
-               new_wikitext = re.sub(r"(?!'')'(\w+)(?!'')'(?!\w)", r'"\1"', wikitext)
-               if new_wikitext != wikitext:
-                    change_count += 1
-                    edit_summaries.append("' -> \" per [[Wikipedia:Manual of Style#Quotation_marks]]")
-                    wikitext = new_wikitext
-               """
+            new_wikitext = re.sub(u"[“”]", r'"', wikitext)
+            # U+201C, U+201D
+            if new_wikitext != wikitext:
+                change_count += 1
+                edit_summaries.append('“” -> " per [[Wikipedia:Manual of Style#Quotation_marks]]')
+                wikitext = new_wikitext
 
-          output_wikitext += wikitext
-     edit_summary = "; ".join(edit_summaries)
-     return (edit_summary, change_count, output_wikitext)
+            """
+            new_wikitext = re.sub(r"(?!'')'(\w+)(?!'')'(?!\w)", r'"\1"', wikitext)
+            if new_wikitext != wikitext:
+                 change_count += 1
+                 edit_summaries.append("' -> \" per [[Wikipedia:Manual of Style#Quotation_marks]]")
+                 wikitext = new_wikitext
+            """
+
+        output_wikitext += wikitext
+    edit_summary = "; ".join(edit_summaries)
+    return (edit_summary, change_count, output_wikitext)
 
 
 def segmentize(input_wikitext):
-     if "<ref>" in input_wikitext:
-          return [{'type': 'ref',
-                  'wikitext': input_wikitext}]
+    if "<ref>" in input_wikitext:
+        return [{'type': 'ref',
+                 'wikitext': input_wikitext}]
 
-     if "<!--" in input_wikitext:
-          return [{'type': 'comment',
-                  'wikitext': input_wikitext}]
+    if "<!--" in input_wikitext:
+        return [{'type': 'comment',
+                 'wikitext': input_wikitext}]
 
-     return [{'type': 'normal',
+    return [{'type': 'normal',
              'wikitext': input_wikitext}]
 
 
