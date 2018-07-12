54b9c6bf45af9df6c29b819a4ad8e7ff2ddbc560
Christopher Beland <beland@alum.mit.edu>
2018-07-12 16:13:47 -0400

    Finish implementation of histogram_text.py

--- histogram_text.py
+++ histogram_text.py
@@ -1,26 +1,48 @@
 import fileinput
 import math
 import re
-
+import sys
 
 number_re = re.compile("\* (\d+)")
 count_by_number_of_things = {}
 
 
+def chop_key(bucket_name):
+    bucket_parts = bucket_name.split("-")
+    return int(bucket_parts[0])
+
+
 def dump_output(hash_to_dump):
-    # for key in sorted(hash_to_dump.keys()):
-        # print("%s:%s" % (key, hash_to_dump[key]))
+    # Consolidate report into ranges (e.g. 741 articles have between 10
+    # and 19 misspelled words)
     bucketed_hash = {}
     for key in hash_to_dump.keys():
-        order_of_magnitude = int(math.log10(key))
-        # first_digit = int(key / 10 ** order_of_magnitude)
-        bucket_lower = key * 10 ** order_of_magnitude
-        bucket_upper = ((key + 1) * 10 ** order_of_magnitude) - 1
-        bucket = "%s-%s" % (bucket_lower, bucket_upper)
-        new_count = bucketed_hash.get(bucket, 0)
-        bucketed_hash[bucket] = new_count
+        print("%s:%s" % (key, hash_to_dump[key]), file=sys.stderr)
+        bucket = None
+        if key == 0:
+            bucket = "0"
+        else:
+            order_of_magnitude = int(math.log10(key))
+            first_digit = int(key / 10 ** order_of_magnitude)
+            bucket_lower = first_digit * 10 ** order_of_magnitude
+            bucket_upper = ((first_digit + 1) * 10 ** order_of_magnitude) - 1
+
+            if bucket_lower == bucket_upper:
+                bucket = str(bucket_lower)
+            else:
+                bucket = "%s-%s" % (bucket_lower, bucket_upper)
+
+        print("BUCKET: %s" % bucket, file=sys.stderr)
+        bucketed_hash[bucket] = bucketed_hash.get(bucket, 0) + hash_to_dump[key]
+
+    # Print out bucketed results
+    for bucket_name in sorted(bucketed_hash.keys(), key=chop_key):
+        print("%s:%s" % (bucket_name, bucketed_hash[bucket_name]))
 
 
+# Consoldate individual counts into number of things with that count
+# (e.g. how many articles have 1 misspelled word, 2 misspelled words,
+# etc.)
 for line in fileinput.input():
     match_result = number_re.match(line)
 
