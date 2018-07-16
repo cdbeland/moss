4933ad450e8347921e9a107fa3d0af83888ce634
Christopher Beland <beland@alum.mit.edu>
2018-07-16 15:14:46 -0400

    Put more non-English misspellings on different channels (to the degree possible), count articles that can't be parsed

--- run_moss.sh
+++ run_moss.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/bash
 
+# Run time for commit e20e070: 19h 47m (whata)
+# Run time: 12h 38m (whata)
+# Run time for commit e317dab: 3 hours (free-spin)
+
 set -e
 
 RUN_NAME=run-`git log | head -c 14 | perl -pe "s/commit //"`+`date "+%Y-%m-%dT%T"`
 
 mkdir $RUN_NAME
 venv/bin/python3 moss_spell_check.py > $RUN_NAME/tmp-output.txt
-# Run time: 12h 38m (whata)
-# Run time for commit e317dabb: 3 hours (free-spin)
 
 cd $RUN_NAME
 grep ^@ tmp-output.txt | sort -nr -k2 > tmp-articles-with-words.txt
@@ -23,20 +25,25 @@ grep -P ' - [a-z ]+$' tmp-articles-linked-words.txt | head -1000 > post-articles
 grep -vP ' - [a-z ]+$' tmp-articles-linked-words.txt | head -1000 > debug-articles-with-most-misspelled-words-intl.txt
 # TODO: Download titles only for Wikipedias for all languages, and
 # include these in all_words lookup (might need to move to database
-# instead of in-memory since this list will get extremely large)
+# instead of in-memory since this list will get extremely large) so
+# -intl channels become useful.  (May also need to remove HTML
+# entities and tags.)
 
-
-tac tmp-articles-linked-words.txt | grep -P "\* 1 -" | perl -pe 's/ - (\w+)$/ - [[wikt:\1]]/' > post-articles-with-single-typo.txt
+tac tmp-articles-linked-words.txt | grep -P "\* 1 -" | grep -P ' - [a-z ]+$' | perl -pe 's/ - (\w+)$/ - [[wikt:\1]]/' > post-articles-with-single-typo.txt
+tac tmp-articles-linked-words.txt | grep -P "\* 1 -" | grep -vP ' - [a-z ]+$' | perl -pe 's/ - (\w+)$/ - [[wikt:\1]]/' > debug-articles-with-single-typo-intl.txt
 
 grep -P "\[\[wikt:[a-z]+\]\]" tmp-words-with-articles.txt | head -1000 | ../venv/bin/python3 ../summarizer.py --find-all > post-most-common-misspellings.txt
 tac tmp-words-with-articles.txt | grep -P "\[\[wikt:[a-z]+\]\]" | head -1000 | ../venv/bin/python3 ../summarizer.py > post-least-common-misspellings.txt
-grep -P "\[\[wikt:&" tmp-words-with-articles.txt | head -1000 | ../venv/bin/python3 ../summarizer.py --find-all > post-most-common-html-entities.txt
-grep -P "\[\[wikt:<" tmp-words-with-articles.txt | head -1000 | ../venv/bin/python3 ../summarizer.py --find-all > post-most-common-html-tags.txt
+grep -P "\[\[wikt:&" tmp-words-with-articles.txt | ../venv/bin/python3 ../summarizer.py --find-all > post-html-entities-by-freq.txt
+grep -P "\[\[wikt:<" tmp-words-with-articles.txt | ../venv/bin/python3 ../summarizer.py --find-all > post-html-tags-by-freq.txt
 
-cat tmp-words-with-articles.txt | grep -vP "\[\[wikt:[a-z]+\]\]" | grep -vP "\[\[wikt:&" | grep -vP "\[\[wikt:<" | head -1000 | ../venv/bin/python3 ../summarizer.py --find-all > post-most-common-misspellings-intl.txt
+cat tmp-words-with-articles.txt | grep -vP "\[\[wikt:[a-z]+\]\]" | grep -vP "\[\[wikt:&" | grep -vP "\[\[wikt:<" | head -1000 | ../venv/bin/python3 ../summarizer.py --find-all > debug-most-common-misspellings-intl.txt
 
 # TODO: Link directly to articles where these words were detected
-tac tmp-misspelled-words-charlen.txt | uniq | perl -pe 's%(\d+)\t(.*)$%* \1 [https://en.wikipedia.org/w/index.php?search=\2 \2]%' > post-longest-shortest-misspelled-words.txt
+tac tmp-misspelled-words-charlen.txt | grep -P '\t[a-z]+$' | uniq | perl -pe 's%(\d+)\t(.*)$%* \1 [https://en.wikipedia.org/w/index.php?search=\2 \2]%' > post-longest-shortest-misspelled-words.txt
+tac tmp-misspelled-words-charlen.txt | grep -vP '\t[a-z]+$' | uniq | perl -pe 's%(\d+)\t(.*)$%* \1 [https://en.wikipedia.org/w/index.php?search=\2 \2]%' > debug-longest-shortest-misspelled-words-intl.txt
 
 # Generate stats
 cat tmp-articles-linked-words.txt | ../venv/bin/python3 ../histogram_text.py > post-misspellings-per-article.txt
+echo "Parse errors: " >> post-misspellings-per-article.txt
+grep -P "!\tABORT" debug-brackets.txt | wc -l >> post-misspellings-per-article.txt
