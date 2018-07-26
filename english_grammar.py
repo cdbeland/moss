58b9ef75cb2c51cda0bac33ffe9944a0770fd39d
Christopher Beland <beland@alum.mit.edu>
2018-07-26 12:05:44 -0400

    Playing around with NLTK grammars

--- english_grammar.py
+++ english_grammar.py
@@ -77,6 +77,7 @@ closed_lexicon = {
     "DET+ART": ["a", "an", "the"],
     # https://en.wiktionary.org/wiki/Category:English_articles (ignoring archaic)
     "TERM": [".", "!", "?"],
+    "COMMA": [","]
 }
 
 attribute_expansion = {
@@ -108,13 +109,15 @@ phrase_structures = {
 
     "AJP": [
         ["ADJ"],  # green
-        ["ADJ", ",", "ADJ"],   # wet, tired
+        ["ADJ", "COMMA", "ADJ"],   # wet, tired
         ["ADJ", "CON+CO", "ADJ"],  # wet and tired
         # TODO: More adjectives than this are possible, but some
         # configurations are disfavored.  Need to think about this
         # more.
         # Note that order matters in a complicated way:
         # https://en.wikipedia.org/wiki/Adjective#Order
+        ["NUM"],  # five
+        ["NUM", "ADJ"]  # two red
     ],
 
     "PP": [
@@ -151,3 +154,472 @@ phrase_structures = {
     #    ["C", "S"],
     # ]
 }
+
+
+enwiktionary_cat_to_pos = {
+    # Canonical form (lemmas in the dictionary)
+    "English_adjectives": "ADJ",
+    "English_adverbs": "ADV",
+    "English_conjunctions": "CON",
+    "English_determiners": "DET",
+    "English_interjections": "INJ",
+    "English_nouns": "N",
+    "English_proper_nouns": "N",
+    "English_numerals": "NUM",
+    "English_particles": "PAR",
+    "English_postpositions": "PST",
+    "English_prepositions": "PRE",
+    "English_pronouns": "PRO",
+    "English_verbs": "V",
+
+    # Inflected
+    "English_adjective_forms": "ADJ",
+    "English_adjective_comparative_forms": "ADJ",
+    "English_adjective_superlative_forms": "ADJ",
+    "English_adverb_forms": "ADV",
+    "English_adverb_comparative_forms": "ADJ",
+    "English_adverb_superlative_forms": "ADJ",
+    "English_noun_forms": "N",
+    "English_noun_plural_forms": "N",
+    "English_irregular_plurals": "N",
+    'English_irregular_plurals_ending_in_"-e"': "N",
+    'English_invariant_nouns': "N",
+    'English_irregular_plurals_ending_in_"-ae"': "N",
+    'English_irregular_plurals_ending_in_"-ces"': "N",
+    'English_irregular_plurals_ending_in_"-des"': "N",
+    'English_irregular_plurals_ending_in_"-es"': "N",
+    'English_irregular_plurals_ending_in_"-ges"': "N",
+    'English_irregular_plurals_ending_in_"-i"': "N",
+    'English_irregular_plurals_ending_in_"-ves"': "N",
+    'English_plurals_ending_in_"-a"': "N",
+    'English_plurals_ending_in_"-en"': "N",
+    'English_plurals_ending_in_"-es"': "N",
+    'English_plurals_ending_in_"-ies"': "N",
+    "English_proper_noun_forms": "N",
+    "English_proper_noun_plural_forms": "N",
+    "English_verb_forms": "V",
+}
+
+# PARTIALLY DIGESTED DUMP OF CATEGORIES
+#
+# English_English
+# English_Latin
+# English_Pig_Latin_terms
+# English_abbreviations
+# English_abstract_nouns
+# English_acronyms
+# English_act-related_adverbs
+# English_adjective-forming_suffixes
+# English_adjective-noun_compound_nouns
+# English_adjective_comparative_forms
+# English_adjective_forms
+# English_adjective_superlative_forms
+# English_adjectives
+# English_adjectives_commonly_used_as_postmodifiers
+# English_adjectives_ending_in_-en
+# English_adjectives_ending_in_-en_formed_from_a_noun
+# English_adjectives_suffixed_with_-en
+# English_adverb-adjective_phrases
+# English_adverb-forming_suffixes
+# English_adverb-preposition_compound_prepositions
+# English_adverb_comparative_forms
+# English_adverb_superlative_forms
+# English_adverbs
+# English_affixes
+# English_agent_nouns
+# English_alliterative_compounds
+# English_allusions
+# English_animal_commands
+# English_aphetic_forms
+# English_apocopic_forms
+# English_archaic_forms
+# English_archaic_terms
+# English_archaic_third-person_singular_forms
+# English_articles
+# English_aspect_adverbs
+# English_autological_terms
+# English_auxiliary_verb_forms
+# English_auxiliary_verbs
+# English_back-formations
+# English_bahuvrihi_compounds
+# English_basic_words
+# English_blends
+# English_borrowed_terms
+# English_braille_logograms
+# English_buzzwords
+# English_calques
+# English_cant
+# English_cardinal_numbers
+# English_case_citation_abbreviations
+# English_catachreses
+# English_catchphrases
+# English_catenative_verbs
+# English_causative_verbs
+# English_childish_terms
+# English_circumfixes
+# English_class_1_strong_verbs
+# English_class_2_strong_verbs
+# English_class_3_strong_verbs
+# English_class_4_strong_verbs
+# English_class_5_strong_verbs
+# English_class_6_strong_verbs
+# English_class_7_strong_verbs
+# English_clausal_phrases
+# English_clippings
+# English_clitics
+# English_cognate_expressions
+# English_collateral_adjectives
+# English_collective_nouns
+# English_colloquialisms
+# English_combining_forms
+# English_comparative-only_adjectives
+# English_compound_adjectives
+# English_compound_determinatives
+# English_compound_words
+# English_conjunctions
+# English_conjunctive_adverbs
+# English_contractions
+# English_contranyms
+# English_control_verbs
+# English_coordinated_pairs
+# English_coordinated_triples
+# English_coordinates
+# English_coordinating_conjunctions
+# English_copulative_verbs
+# English_countable_nouns
+# English_countable_proper_nouns
+# English_dated_forms
+# English_dated_terms
+# English_defective_verbs
+# English_degree_adverbs
+# English_deixes
+# English_demonstrative_adverbs
+# English_derogatory_terms
+# English_determiners
+# English_dialectal_terms
+# English_diminutive_nouns
+# English_diminutive_proper_nouns
+# English_diminutives_of_female_given_names
+# English_diminutives_of_male_given_names
+# English_discourse_markers
+# English_dismissals
+# English_disputed_terms
+# English_ditransitive_verbs
+# English_domain_adverbs
+# English_double_contractions
+# English_doublets
+# English_duration_adverbs
+# English_dvandva_compounds
+# English_ellipses
+# English_elongated_forms
+# English_eponyms
+# English_ergative_verbs
+# English_ethnic_slurs
+# English_euphemisms
+# English_evaluative_adverbs
+# English_familiar_terms
+# English_female_given_names
+# English_filled_pauses
+# English_first-person_singular_forms
+# English_first_person_pronouns
+# English_focus_adverbs
+# English_formal_terms
+# English_four-letter_abbreviations
+# English_fractional_numbers
+# English_frequency_adverbs
+# English_frequentative_verbs
+# English_gender-neutral_terms
+# English_genericized_trademarks
+# English_gerunds
+# English_given_names
+# English_hapax_legomena
+# English_haplological_forms
+# English_hedges
+# English_heteronyms
+# English_historical_terms
+# English_homographic_denominal_verbs
+# English_homophonic_translations
+# English_honorific_forms
+# English_hyperboles
+# English_hypercorrections
+# English_hyperforeign_terms
+# English_idiomatic_construction_prototypes
+# English_idioms
+# English_imperative_sentences
+# English_imperatives
+# English_impersonal_verbs
+# English_indefinite_pronouns
+# English_infixes
+# English_inflectional_suffixes
+# English_informal_demonyms
+# English_informal_forms
+# English_informal_terms
+# English_initialisms
+# English_intensifiers
+# English_interfixes
+# English_interjections
+# English_interjs
+# English_interrogative_adverbs
+# English_interrogative_pro-forms
+# English_interrogative_pronouns
+# English_intransitive_verbs
+# English_invariant_nouns
+# English_irregular_first-person_singular_forms
+# English_irregular_past_participles
+# English_irregular_plurals
+# English_irregular_second-person_singular_forms
+# English_irregular_simple_past_forms
+# English_irregular_third-person_singular_forms
+# English_irregular_verbs
+# English_jocular_terms
+# English_karmadharaya_compounds
+# English_learnedly_borrowed_terms
+# English_lemmas
+# English_letters
+# English_light_verb_constructions
+# English_literary_terms
+# English_location_adverbs
+# English_locatives
+# English_male_given_names
+# English_manner_adverbs
+# English_matched_pairs
+# English_medical_slang
+# English_merisms
+# English_metonyms
+# English_military_slang
+# English_minced_oaths
+# English_misconstructions
+# English_misspellings
+# English_mnemonics
+# English_modal_adverbs
+# English_modal_verbs
+# English_negative_polarity_items
+# English_neologisms
+# English_non-constituents
+# English_non-idiomatic_translation_targets
+# English_non-lemma_forms
+# English_nonce_terms
+# English_nonstandard_forms
+# English_nonstandard_terms
+# English_noun-forming_suffixes
+# English_noun-noun_compound_nouns
+# English_noun_forms
+# English_noun_plural_forms
+# English_nouns
+# English_nouns_with_common_ending_formations
+# English_nouns_with_irregular_plurals
+# English_nouns_with_unattested_plurals
+# English_nouns_with_unknown_or_uncertain_plurals
+# English_numbers
+# English_numeral_symbols
+# English_numerals
+# English_numerical_contractions
+# English_oaths
+# English_obsolete_forms
+# English_obsolete_terms
+# English_offensive_terms
+# English_officialese_terms
+# English_one-letter_words
+# English_onomatopoeias
+# English_ordinal_numbers
+# English_orthographically_borrowed_terms
+# English_oxymorons
+# English_palindromes
+# English_parasynthetic_adjectives
+# English_particles
+# English_past_participles
+# English_pejoratives
+# English_personal_pronouns
+# English_phrasal_prepositions
+# English_phrasal_verbs
+# English_phrasal_verbs_with_particle_(aback)
+# English_phrasal_verbs_with_particle_(about)
+# English_phrasal_verbs_with_particle_(above)
+# English_phrasal_verbs_with_particle_(across)
+# English_phrasal_verbs_with_particle_(adrift)
+# English_phrasal_verbs_with_particle_(after)
+# English_phrasal_verbs_with_particle_(against)
+# English_phrasal_verbs_with_particle_(aground)
+# English_phrasal_verbs_with_particle_(ahead)
+# English_phrasal_verbs_with_particle_(aloft)
+# English_phrasal_verbs_with_particle_(along)
+# English_phrasal_verbs_with_particle_(apart)
+# English_phrasal_verbs_with_particle_(around)
+# English_phrasal_verbs_with_particle_(as)
+# English_phrasal_verbs_with_particle_(aside)
+# English_phrasal_verbs_with_particle_(astray)
+# English_phrasal_verbs_with_particle_(asunder)
+# English_phrasal_verbs_with_particle_(at)
+# English_phrasal_verbs_with_particle_(away)
+# English_phrasal_verbs_with_particle_(back)
+# English_phrasal_verbs_with_particle_(before)
+# English_phrasal_verbs_with_particle_(behind)
+# English_phrasal_verbs_with_particle_(below)
+# English_phrasal_verbs_with_particle_(between)
+# English_phrasal_verbs_with_particle_(beyond)
+# English_phrasal_verbs_with_particle_(by)
+# English_phrasal_verbs_with_particle_(down)
+# English_phrasal_verbs_with_particle_(even)
+# English_phrasal_verbs_with_particle_(for)
+# English_phrasal_verbs_with_particle_(forth)
+# English_phrasal_verbs_with_particle_(forward)
+# English_phrasal_verbs_with_particle_(from)
+# English_phrasal_verbs_with_particle_(in)
+# English_phrasal_verbs_with_particle_(into)
+# English_phrasal_verbs_with_particle_(low)
+# English_phrasal_verbs_with_particle_(of)
+# English_phrasal_verbs_with_particle_(off)
+# English_phrasal_verbs_with_particle_(on)
+# English_phrasal_verbs_with_particle_(onto)
+# English_phrasal_verbs_with_particle_(out)
+# English_phrasal_verbs_with_particle_(over)
+# English_phrasal_verbs_with_particle_(past)
+# English_phrasal_verbs_with_particle_(round)
+# English_phrasal_verbs_with_particle_(through)
+# English_phrasal_verbs_with_particle_(to)
+# English_phrasal_verbs_with_particle_(together)
+# English_phrasal_verbs_with_particle_(towards)
+# English_phrasal_verbs_with_particle_(under)
+# English_phrasal_verbs_with_particle_(up)
+# English_phrasal_verbs_with_particle_(upon)
+# English_phrasal_verbs_with_particle_(with)
+# English_phrasal_verbs_with_particle_(without)
+# English_phrasebook
+# English_phrasebook/Communication
+# English_phrasebook/Emergencies
+# English_phrasebook/Ethnicity
+# English_phrasebook/Family
+# English_phrasebook/Food_and_drink
+# English_phrasebook/Health
+# English_phrasebook/Love
+# English_phrasebook/Religion
+# English_phrasebook/Sex
+# English_phrasebook/Travel
+# English_phrases
+# English_placeholder_terms
+# English_pleonastic_compound_adjectives
+# English_pleonastic_compound_nouns
+# English_plural_pronouns
+# English_pluralia_tantum
+# English_plurals
+# English_plurals_ending_in_"-a"
+# English_plurals_ending_in_"-en"
+# English_plurals_ending_in_"-es"
+# English_plurals_ending_in_"-ies"
+# English_poetic_terms
+# English_polite_terms
+# English_possessive_determiners
+# English_possessive_pronouns
+# English_post-nominal_letters
+# English_postpositions
+# English_predicates
+# English_prefixes
+# English_prepositional_phrases
+# English_prepositions
+# English_present_participles
+# English_pro-forms
+# English_pro-sentences
+# English_productive_suffixes
+# English_pronominal_adverbs
+# English_pronouns
+# English_pronunciation_spellings
+# English_proper_adjectives
+# English_proper_names
+# English_proper_noun_plural_forms
+# English_proper_nouns
+# English_proper_nouns_with_unattested_plurals
+# English_proper_nouns_with_unknown_or_uncertain_plurals
+# English_proverbs
+# English_pseudo-acronyms
+# English_punctuation_marks
+# English_quantizers
+# English_questions
+# English_radio_slang
+# English_rare_forms
+# English_rare_terms
+# English_rebracketings
+# English_reciprocal_pronouns
+# English_reciprocal_verbs
+# English_reduplicated_coordinated_pairs
+# English_reduplicated_coordinated_quadruples
+# English_reduplicated_coordinated_triples
+# English_reduplications
+# English_reflexive_pronouns
+# English_reflexive_verbs
+# English_refractory_feminine_rhymes
+# English_relative_pronouns
+# English_religious_slurs
+# English_reporting_verbs
+# English_responses
+# English_retronyms
+# English_rhetorical_questions
+# English_rhyming_compounds
+# English_rhyming_slang
+# English_sarcastic_terms
+# English_school_slang
+# English_second-person_singular_forms
+# English_second-person_singular_past_tense_forms
+# English_second_person_pronouns
+# English_senses_used_only_in_hyphenated_compounds
+# English_sentence_adverbs
+# English_sentences
+# English_sequence_adverbs
+# English_set_phrases
+# English_short_forms
+# English_similes
+# English_singularia_tantum
+# English_slang
+# English_speech-act_adverbs
+# English_spoonerisms
+# English_stative_verbs
+# English_student_slang
+# English_subordinate_clauses
+# English_subordinating_conjunctions
+# English_suffix_forming_adjectives_suffixed_with_-al
+# English_suffix_forms
+# English_suffixes
+# English_suffixes_suffixed_with_-ic
+# English_suffixes_that_form_adjectives_from_nouns
+# English_suffixes_that_form_nouns_from_adjectives
+# English_suppletive_adjectives
+# English_suppletive_adverbs
+# English_suppletive_nouns
+# English_suppletive_verbs
+# English_surnames
+# English_swear_words
+# English_syllabic_abbreviations
+# English_symbols
+# English_synchronized_entries
+# English_syncopic_forms
+# English_temporal_location_adverbs
+# English_third-person_singular_forms
+# English_third_person_pronouns
+# English_three-letter_abbreviations
+# English_three-letter_words
+# English_time_adverbs
+# English_trademarks
+# English_transitive_verbs
+# English_translation_hubs
+# English_triple_contractions
+# English_twice-borrowed_terms
+# English_two-letter_words
+# English_uncommon_forms
+# English_uncomparable_adjectives
+# English_uncomparable_adverbs
+# English_uncountable_nouns
+# English_unisex_given_names
+# English_univerbations
+# English_unproductive_prefixes
+# English_unproductive_suffixes
+# English_verb-forming_suffixes
+# English_verb_forms
+# English_verb_forms_using_redundant_wikisyntax
+# English_verb_froms
+# English_verb_irregular_forms
+# English_verb_simple_past_forms
+# English_verbal_nouns
+# English_verbs
+# English_verbs_suffixed_with_-en
+# English_verbs_with_base_form_identical_to_past_participle
+# English_verbs_with_placeholder_it
+# English_verbs_with_weak_preterite_but_strong_past_participle
--- grammar.py
+++ grammar.py
@@ -12,23 +12,20 @@
 #   * Command line from file
 #   * Command line from Wikipedia API
 #   * JavaScript
-# * Produce reports for:
-#   * Words to add to Wiktionary
-#   * Wikipedia articles to fix
-#     * Check e.g. that misspelled words are still present in live article
-#   * Wikipedia common typos to fix
 # * Report on theory of English grammar
 
-# A quasi-representative sample of topics (for postive testing) from
-# well-written articles listed at:
-# https://en.wikipedia.org/wiki/Wikipedia:Featured_articles
 
-
+import mysql.connector
 import nltk
 import re
 import wikipedia
 # https://wikipedia.readthedocs.io/en/latest/code.html
-from spell import is_word_spelled_correctly
+from english_grammar import enwiktionary_cat_to_pos, phrase_structures, closed_lexicon
+
+
+mysql_connection = mysql.connector.connect(user='beland',
+                                           host='127.0.0.1',
+                                           database='enwiktionary')
 
 
 # TODO: Use this for visualization.  Would be interesting to see
@@ -55,6 +52,7 @@ def generate_stats(plaintext):
 # TODO: Report as bug to either Mediawiki (check for existing bug) or
 # Python Wikipedia module.
 # POSSIBLE WORKAROUND: Use wikitext instead of plain text
+# ACTUALLY TODO - run through wiki_utils.py
 broken_re = re.compile(r"displaystyle|{|}")
 
 
@@ -73,6 +71,8 @@ def check_english(plaintext, title):
         print("!\tArticle parse broken?\t%s" % title)
         return
 
+    (grammar_string, word_to_pos) = load_grammar_for_page(plaintext)
+
     paragraphs = plaintext.split("\n")
     for paragraph in paragraphs:
 
@@ -86,26 +86,78 @@ def check_english(plaintext, title):
         for sentence in sentences:
             words = nltk.word_tokenize(sentence)
             if len(words) > 200:
-                print("S\t%s\tOverly long sentence?\t%s words\t%s" % (title, len(words), sentence))
+                print("L\t%s\tOverly long sentence?\t%s words\t%s" % (title, len(words), sentence))
 
-            unknown_word = False
-            for word in words:
-                if not is_word_spelled_correctly(word):
-                    # Suppression of foreign words with {{lang}} and
-                    # {{not a typo}} doesn't work with the plain text,
-                    # only with wikitext.  So just use this as a quick
-                    # filter to skip sentences with words we can't
-                    # identify or junk that isn't words.
-                    print("S\t%s\tSkipping sentence due to unknown word\t%s\t%s" % (title, word, sentence))
-                    unknown_word = True
-            if unknown_word:
-                continue
-            if not is_sentence_grammatical(sentence):
+            # TODO: Skip this inside <poem>...</poem>
+            if not is_sentence_grammatical_beland(words, word_to_pos, title, sentence, grammar_string):
                 print("G\t%s\tUngrammatical sentence?\t%s" % (title, sentence))
 
 
-def is_sentence_grammatical(sentence):
-    # TODO!
+def is_sentence_grammatical_beland(word_list, word_to_pos, title, sentence, grammar_string):
+    if "==" in word_list or "===" in word_list:
+        return True
+
+    word_train = []
+
+    first_word = True
+    for word in word_list:
+        expand_grammar = False
+        pos_list = word_to_pos.get(word)
+        if pos_list:
+            print("Found %s: %s" % (word, pos_list))
+        if first_word and not pos_list and word == word.title():
+            pos_list = word_to_pos.get(word.lower())
+            if pos_list:
+                print("Lowercase version found for %s" % word)
+                # For CFG library
+                word_list[0] = word.lower()
+        if not pos_list and word.isalpha() and (word == word.title() or word == word.upper()):
+            # Assume all capitalized words and acronyms are proper nouns
+            pos_list = ["N"]
+            expand_grammar = True
+            print("Assuming proper noun for %s" % word)
+        if not pos_list and word.isnumeric():
+            pos_list = ["NUM"]
+            expand_grammar = True
+            print("Assuming number for %s" % word)
+        if not pos_list:
+            # print("S\t%s\tSkipping sentence due to unknown word\t%s\t%s" % (title, word, word_list))
+            print("S\t%s\tNo POS for word\t%s" % (word, word_list))
+            return True
+
+        if expand_grammar:
+            print("expanding %s %s" % (word, pos_list))
+            word_to_pos[word] = pos_list
+            for pos in pos_list:
+                grammar_string += '%s -> "%s"\n' % (pos, word)
+
+        word_train.append((word, pos_list))
+        first_word = False
+
+    # print("DEBUG\t%s" % word_train)
+
+    grammar = nltk.CFG.fromstring(grammar_string)
+    parser = nltk.parse.RecursiveDescentParser(grammar)
+
+    try:
+        possible_parses = parser.parse(word_list)
+    except Exception as e:
+        # print(grammar_string)
+        print(e)
+        return False
+
+    if possible_parses:
+        print([parse for parse in possible_parses])
+        return True
+    else:
+        return False
+
+
+def is_sentence_grammatical_nltk(word_list):
+    # "word_list" instead of sentence avoids re-tokenizing
+
+    tagged = nltk.pos_tag(word_list)
+    print(tagged)
     return True
 
 
@@ -121,6 +173,10 @@ def check_article(title):
 
 
 # BOOTSTRAPPING
+
+# A quasi-representative sample of topics (for positive testing) from
+# well-written articles listed at:
+# https://en.wikipedia.org/wiki/Wikipedia:Featured_articles
 
 sample_featured_articles = [
     "BAE Systems",
@@ -170,9 +226,6 @@ def check_samples_from_disk():
             # generate_stats(plaintext)
 
 
-check_samples_from_disk()
-
-
 def save_sample_articles():
     for title in sample_featured_articles:
         plaintext = fetch_article_plaintext(title)
@@ -181,4 +234,69 @@ def save_sample_articles():
             text_file.write(plaintext)
 
 
+# --- GRAMMAR-MAKING HACK ---
+
+
+def fetch_categories(word):
+    if not word:
+        return []
+    cursor = mysql_connection.cursor()
+    cursor.execute("SELECT title, category_name FROM page_categories WHERE title=%s", (word, ))
+
+    # Account for SQL being case-insensitive
+    result = [cat.decode('utf-8') for (title, cat) in cursor if title.decode('utf-8') == word]
+    cursor.close()
+    return result
+
+
+def load_grammar_for_page(page_text):
+    grammar_string = ""
+
+    word_set = set(nltk.word_tokenize(page_text))
+
+    more_words = set()
+    for word in word_set:
+        if word == word.title():
+            more_words.add(word.lower())
+    word_set = word_set.union(more_words)
+
+    word_to_pos = {}
+
+    for word in word_set:
+        word_categories = fetch_categories(word)
+        word_pos_set = set()
+        for category_name in word_categories:
+            new_pos = enwiktionary_cat_to_pos.get(category_name)
+            if new_pos:
+                word_pos_set.add(new_pos)
+
+        if word_pos_set:
+            word_to_pos[word] = list(word_pos_set)
+
+        for pos in word_pos_set:
+            grammar_string += '%s -> "%s"\n' % (pos, word)
+
+    for (parent_pos, child_structures) in phrase_structures.items():
+        for child_list in child_structures:
+            # Strip attributes for now
+            child_list = [child.split("+")[0] for child in child_list]
+            grammar_string += "%s -> %s\n" % (parent_pos, " ".join(child_list))
+
+    for (pos, word_list) in closed_lexicon.items():
+        pos = pos.split("+")[0]
+        for word in word_list:
+            grammar_string += '%s -> "%s"\n' % (pos, word)
+
+            pos_list = word_to_pos.get(word, [])
+            pos_list.append(pos)
+            word_to_pos[word] = pos_list
+
+    return (grammar_string, word_to_pos)
+
+
+# --- RUNTIME ---
+
 # save_sample_articles()
+
+check_samples_from_disk()
+mysql_connection.close()
