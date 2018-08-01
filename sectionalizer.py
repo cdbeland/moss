ede45597fe38c9b91042c4b93415074aae5bdb54
Christopher Beland <beland@alum.mit.edu>
2018-08-01 15:43:45 -0400

    sectionalizer.py: Merge small sections, and allow 5-char keys since now having lots of tiny sections is not a problem

--- sectionalizer.py
+++ sectionalizer.py
@@ -2,6 +2,11 @@ import fileinput
 import re
 
 
+MAX_SIZE = 50
+MIN_SIZE = 15
+MAX_KEY_LENGTH = 5
+
+
 def get_word(line):
     match_result = re.search(r"\[\[(.*?)\]\]",
                              line)
@@ -17,23 +22,51 @@ for line in fileinput.input("-"):
     lines.append(line)
     grouped_lines[word[0]] = lines
 
-changes_made = True
-while changes_made:
-    changes_made = False
+
+def split_big_sections(grouped_lines):
+    changes_made = True
+    while changes_made:
+        changes_made = False
+        new_hash = {}
+        for (group_key, line_list) in grouped_lines.items():
+            if len(line_list) > MAX_SIZE and len(group_key) < MAX_KEY_LENGTH:
+                changes_made = True
+                for line in line_list:
+                    word = get_word(line)
+                    new_key = word[0:len(group_key) + 1]
+                    lines = new_hash.get(new_key, [])
+                    lines.append(line)
+                    new_hash[new_key] = lines
+            else:
+                new_hash[group_key] = line_list
+        grouped_lines = new_hash
+    return new_hash
+
+
+def merge_small_sections(grouped_lines):
     new_hash = {}
-    for (group_key, line_list) in grouped_lines.items():
-        if len(line_list) > 50 and len(group_key) < 5:
-            changes_made = True
-            for line in line_list:
-                word = get_word(line)
-                new_key = word[0:len(group_key) + 1]
-                lines = new_hash.get(new_key, [])
-                lines.append(line)
-                new_hash[new_key] = lines
+    previous_key = None
+    for (group_key, line_list) in sorted(grouped_lines.items()):
+        if len(line_list) < MIN_SIZE and previous_key:
+            tmp_list = new_hash[previous_key]
+            if len(tmp_list) < MAX_SIZE - MIN_SIZE:
+                tmp_list.extend(line_list)
+                del new_hash[previous_key]
+                key_parts = previous_key.split("-")
+                new_key = "%s-%s" % (key_parts[0], group_key)
+                new_hash[new_key] = tmp_list
+                previous_key = new_key
+            else:
+                new_hash[group_key] = line_list
+                previous_key = group_key
         else:
             new_hash[group_key] = line_list
-    grouped_lines = new_hash
+            previous_key = group_key
+    return new_hash
 
+
+grouped_lines = split_big_sections(grouped_lines)
+grouped_lines = merge_small_sections(grouped_lines)
 for (group_key, line_list) in sorted(grouped_lines.items()):
     print("==== %s ====" % group_key)
     for line in sorted(line_list):
