816c025d594b80d0fc4a789907bfd46eee40ca9e
Christopher Beland <beland@alum.mit.edu>
2018-09-11 18:51:23 -0400

    update_downloads.sh: Automatically load new dumps into local MySQL database

--- update_downloads.sh
+++ update_downloads.sh
@@ -30,3 +30,39 @@ gunzip specieswiki-latest-all-titles-in-ns0.gz
 rm -f enwiki-latest-pages-articles-multistream.xml.bz2
 wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles-multistream.xml.bz2
 bunzip2 enwiki-latest-pages-articles-multistream.xml.bz2
+
+# FIRST TIME SETUP
+
+# su
+# service mariadb restart
+# mysql
+#  CREATE DATABASE enwiktionary;
+#  GRANT ALL PRIVILEGES ON enwiktionary TO 'beland'@'localhost' WITH GRANT OPTION;
+
+
+cat enwiktionary-latest-categorylinks.sql | mysql -D enwiktionary
+# Load time: About 1 hour 10 minutes
+
+cat enwiktionary-latest-page.sql | mysql -D enwiktionary
+# Load time: About 25 minutes
+
+echo "DROP TABLE IF EXISTS page_categories;" | mysql -D enwiktionary
+echo "CREATE TABLE page_categories (
+  title varbinary(255) NOT NULL DEFAULT '',
+  category_name varbinary(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (title, category_name)
+);" | mysql -D enwiktionary
+
+echo "DELETE FROM page WHERE page_namespace != 0;" | mysql -D enwiktionary
+# Query OK, 569828 rows affected (2 min 30.10 sec)
+
+echo "INSERT INTO page_categories (title, category_name)
+ SELECT page_title, cl_to
+ FROM page, categorylinks
+ WHERE page.page_id = categorylinks.cl_from;" | mysql -D enwiktionary
+# Query OK, 22540237 rows affected (16 min 3.32 sec)
+
+echo "ALTER TABLE page_categories ADD INDEX i_title (title);" | mysql -D enwiktionary
+# Query OK, 0 rows affected (5 min 27.56 sec)         
+echo "ALTER TABLE page_categories ADD INDEX i_cat (category_name);" | mysql -D enwiktionary
+# Query OK, 0 rows affected (5 min 49.56 sec)         
