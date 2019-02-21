e3b6c88062605cb1c71de14d43fc8fc9e437099e
Christopher Beland <beland@alum.mit.edu>
2019-02-21 00:31:41 -0500

    Collect more TS since they seem popular with editors

--- run_moss.sh
+++ run_moss.sh
@@ -114,7 +114,7 @@ echo "" >> collected_by_length.txt
 # grep -P "^T1\t" tmp-misspelled-words-charlen-cat.txt | perl -pe 's/^T1\t//' | head -20 >> collected_by_length.txt
 
 echo "==== Likely missing whitespace ====" >> collected_by_length.txt
-grep ^TS tmp-misspelled-words-charlen-cat.txt | perl -pe 's/^TS\t//' | head -20 >> collected_by_length.txt
+grep ^TS tmp-misspelled-words-charlen-cat.txt | perl -pe 's/^TS\t//' | head -200 >> collected_by_length.txt
 
 echo "" >> collected_by_length.txt
 echo "==== Likely chemistry words ====" >> collected_by_length.txt
