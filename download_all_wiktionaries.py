32f7885f4e49dff2467e1e4fa9ae2d8d134ab202
Christopher Beland <beland@alum.mit.edu>
2019-07-24 10:05:04 -0400

    Update download_all_wiktionaries.py for live HTML/CSS changes

--- download_all_wiktionaries.py
+++ download_all_wiktionaries.py
@@ -3,10 +3,11 @@ import urllib.request
 
 
 directory_html = urllib.request.urlopen("https://dumps.wikimedia.org/backup-index-bydb.html").read().decode('utf-8')
-language_codes = re.findall(r"([_a-z]+)wiktionary</a>: <span class='done'>", directory_html)
+print(directory_html)
+language_codes = re.findall(r"([_a-z]+)wiktionary</a>: <span class='(done|partial-dump)'>", directory_html)
 # Excludes closed wiktionaries
 
-for language_code in language_codes:
+for (language_code, _dump_status) in language_codes:
     file_name = "%swiktionary-latest-all-titles-in-ns0.gz" % language_code
     dump_url = "https://dumps.wikimedia.org/%swiktionary/latest/%s" % (language_code, file_name)
     print("Downloading %s" % dump_url)
