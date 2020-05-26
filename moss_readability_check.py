abeddb2b00828925615e2ef98aeba6bf00f47849
Christopher Beland <beland@alum.mit.edu>
2020-05-26 14:13:21 -0400

    Readability: Ignore articles marked for cleanup, and with a Dale-Chall score < 12

--- moss_readability_check.py
+++ moss_readability_check.py
@@ -5,6 +5,7 @@ import re
 import textstat
 import sys
 from moss_dump_analyzer import read_en_article_text
+from moss_spell_check import ignore_tags_re
 from wikitext_util import wikitext_to_plaintext, get_main_body_wikitext
 
 
@@ -41,6 +42,8 @@ def check_reading_level(article_title, article_text):
         return
     if article_text.startswith("#REDIRECT") or article_text.startswith("# REDIRECT"):
         return
+    if ignore_tags_re.search(article_text):
+        return
 
     article_text = wikitext_to_plaintext(article_text)
     article_text = get_main_body_wikitext(article_text, strong=True)
@@ -57,7 +60,18 @@ def check_reading_level(article_title, article_text):
         # Ratings of very short articles are unreliable.
         return
 
+    # TODO: Replace this with a sentence length metric, which is
+    # faster to calculate and easier to set an absolute threshold for.
     article_difficulty_level = textstat.dale_chall_readability_score(article_text)
+
+    # After consideration by the Guild of Copy Editors, we decided not
+    # to bother trying to make a systematic attempt to reduce the
+    # reading level of articles.  So for now we only care about
+    # articles that are excessively high on the scale, which probably
+    # need to be marked for cleanup.
+    if article_difficulty_level < 12:
+        return
+
     print(f"* {article_difficulty_level} - [[{article_title}]]")
 
 
