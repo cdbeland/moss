4f3ee0529e9ee93df9ddfbec89acc9bbfdbbca25
Christopher Beland <beland@alum.mit.edu>
2020-05-27 22:25:06 -0400

    Allow R groups in chemical formulas

--- word_categorizer.py
+++ word_categorizer.py
@@ -109,7 +109,9 @@ element_symbols = [
     "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os",
     "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa",
     "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg",
-    "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"]
+    "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og",
+    "R"  # Not an element; for [[substiuent]]s ("R groups")
+]
 
 chem_roman_numerals = ["II", "III", "IV", "VI", "VII", "VIII", "IX"]
 
