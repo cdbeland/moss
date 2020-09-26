a26cccace7124e6bba4869d2b5e99bc26e24c9b6
Christopher Beland <beland@alum.mit.edu>
2020-09-26 03:05:58 -0400

    More experimenting with rollup_ignored.py

--- rollup_ignored.py
+++ rollup_ignored.py
@@ -2,18 +2,18 @@ from collections import defaultdict
 import fileinput
 import re
 
-unsupported_titles = ["{", "}", "|", "#", ".", ":", "<", ">", "__TOC__", "||"]
-
+UNSUPPORTED_TITLES = ["{", "}", "|", "#", ".", ":", "<", ">", "__TOC__", "||"]
+PROPER_NOUN_RE = re.compile("^[A-Z][a-z]+$")
 
 if __name__ == '__main__':
 
     articles_by_word = defaultdict(list)
-    
+
     for line in fileinput.input("-"):
         line = line.strip()
 
         (_, word, article) = line.split("\t")
-        if word in unsupported_titles:
+        if word in UNSUPPORTED_TITLES:
             continue
         articles_by_word[word].append(article)
 
@@ -23,6 +23,8 @@ if __name__ == '__main__':
         category = "*"
         if word.endswith("’s"):
             category = "S"  # MOS:STRAIGHT
+        if PROPER_NOUN_RE.match(word):
+            category = "P"
 
         # article_links = ", ".join(["[[%s]]" % article_name for article_name in sorted(article_set)][0:500])
         # Limit at 500 to avoid expensive sorting.  Sampled articles
