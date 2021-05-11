caf089452f1fa0de7479b69bb81372357bde2791
Christopher Beland <beland@alum.mit.edu>
2021-05-11 14:19:30 -0400

    Readability: ignore more articles with patterns and article_skip_list; increase maximum tolerable reading level to 12.5, to avoid getting too many probably-OK articles

--- moss_readability_check.py
+++ moss_readability_check.py
@@ -11,6 +11,11 @@ from wikitext_util import wikitext_to_plaintext, get_main_body_wikitext
 
 list_cleanup_re = re.compile(r"(^|\n)[ :#\*].*")
 year_in_re = re.compile(r"^(\d+s?$|\d+s? BC$|\d\d+s? in )")
+article_skip_list = [
+    "Globotriaosylceramide 3-beta-N-acetylgalactosaminyltransferase",
+    "Princely houses of Poland and Lithuania",
+    "Alfonso de Borbón y Borbón",
+]
 
 
 def is_prose_paragraph(paragraph_text):
@@ -44,10 +49,29 @@ def check_reading_level(article_title, article_text):
         return
     if article_text.startswith("#REDIRECT") or article_text.startswith("# REDIRECT"):
         return
-    if article_title.endswith("season") or article_title.endswith("roster") or article_title.endswith("squads"):
+    if (article_title.endswith("season")
+            or article_title.endswith("roster")
+            or article_title.endswith("rosters")
+            or article_title.endswith("League")
+            or article_title.endswith("Games")
+            or article_title.endswith("Cup")
+            or article_title.endswith("football")
+            or article_title.endswith("squads")):
         if article_title[0:4].isnumeric():
             return
+        if article_title[0:3] == "FC ":
+            return
+        if "at the" in article_title:
+            return
+    if article_title.endswith("Championships"):
+        return
     if ignore_tags_re.search(article_text):
+        return
+    if "{{like resume" in article_text:
+        return
+    if "{{Advert" in article_text:
+        return
+    if article_title in article_skip_list:
         return
 
     article_text = wikitext_to_plaintext(article_text)
@@ -74,7 +98,7 @@ def check_reading_level(article_title, article_text):
     # reading level of articles.  So for now we only care about
     # articles that are excessively high on the scale, which probably
     # need to be marked for cleanup.
-    if article_difficulty_level < 12:
+    if article_difficulty_level < 12.5:
         return
 
     print(f"* {article_difficulty_level} - [[{article_title}]]")
