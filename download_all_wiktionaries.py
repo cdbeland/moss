86e7022780bc3a09b32039595e7030933ea147f9
Christopher Beland <beland@alum.mit.edu>
2021-08-08 23:52:51 -0400

    Sleep for 1 second between downloads to avoid weird network errors

--- download_all_wiktionaries.py
+++ download_all_wiktionaries.py
@@ -1,3 +1,4 @@
+from time import sleep
 import re
 import urllib.request
 
@@ -9,6 +10,7 @@ language_codes = re.findall(r"([_a-z]+)wiktionary</a>: <span class='(done|partia
 for (language_code, _dump_status) in language_codes:
     file_name = "%swiktionary-latest-all-titles-in-ns0.gz" % language_code
     dump_url = "https://dumps.wikimedia.org/%swiktionary/latest/%s" % (language_code, file_name)
+    sleep(1)
     print("Downloading %s" % dump_url)
     try:
         urllib.request.urlretrieve(dump_url, "/bulk-wikipedia/all-wiktionaries/%s" % file_name)
