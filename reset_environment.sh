6fd6396a7c1ff4a04572a4418819ba846044e3cf
Christopher Beland <beland@alum.mit.edu>
2021-10-16 20:01:48 -0400

    Add gcld3 and pycountry as module requirements (for language identification)

--- reset_environment.sh
+++ reset_environment.sh
@@ -1,6 +1,7 @@
 rm -rf venv/
 python3 -m venv venv
 venv/bin/pip install --upgrade pip
+venv/bin/pip install wheel
 venv/bin/pip install -r requirements.txt
-venv/bin/python3 -c "import nltk; nltk.download('punkt')"
-venv/bin/python3 -c "import nltk; nltk.download('averaged_perceptron_tagger')"
+venv/bin/python -c "import nltk; nltk.download('punkt')"
+venv/bin/python -c "import nltk; nltk.download('averaged_perceptron_tagger')"
