a7bec8463e53628c8e6785798de7c7f02152e6c8
Christopher Beland <beland@alum.mit.edu>
2021-10-29 18:50:15 -0400

    Switch chemical_formula_report.py to using wikitext_to_plaintext()

--- chemical_formula_report.py
+++ chemical_formula_report.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 import mysql.connector
 import re
 from moss_dump_analyzer import read_en_article_text
-
+from wikitext_util import wikitext_to_plaintext, get_main_body_wikitext
 
 mysql_connection = mysql.connector.connect(user='beland',
                                            host='127.0.0.1',
@@ -21,16 +21,27 @@ for category in [
     formulas.extend([title.decode('utf8') for (title,) in cursor])
 cursor.close()
 
-formulas = sorted(set([f for f in formulas if any(char for char in list(f) if char.isdigit())]))
-supression_re = re.compile(r"{{chem\|.*?}}|{{chem2\|.*?}}")
+formulas = [f for f in formulas if any(char for char in list(f) if char.isdigit())]
+formulas = [f for f in formulas if "," not in f]
+formulas = sorted(set(formulas), key=lambda f: len(f) * -1)
+formulas_pattern = "|".join([rf"\b{formula}\b" for formula in formulas])
+formulas_pattern = formulas_pattern.replace("(", r"\(")
+formulas_pattern = formulas_pattern.replace(")", r"\)")
+formulas_pattern = formulas_pattern.replace("+", r"\+")
+formulas_re = re.compile(formulas_pattern)
+
 articles_by_formula = defaultdict(list)
 
 
 def chem_formula_check(article_title, article_text):
-    article_text = supression_re.sub("", article_text)
+    article_text = wikitext_to_plaintext(article_text)
+    article_text = get_main_body_wikitext(article_text)
+
+    # Fast but over-matches
     matches = [f for f in formulas if f in article_text]
-    # if matches:
-    #     print(f"* [[{article_title}]] - {' '.join(matches)}")
+    if matches:
+        # Slow but fewer false matches
+        matches = formulas_re.findall(article_text)
     return(article_title, matches)
 
 
@@ -42,8 +53,22 @@ def add_tuples_to_results(tuple_list):
         articles_by_formula[match].append(article_title)
 
 
+def dump_results():
+    tuples = list(articles_by_formula.items())
+    tuples.sort(key=lambda t: (len(t[1]) * -1, t[0]))
+    for length_cap in [5, 5000]:
+        for (formula, article_list) in tuples:
+            short_list = sorted(article_list)[0:length_cap]
+            length = len(article_list)
+            line = f"* {length} - {formula} - [["
+            line += "]], [[".join(short_list)
+            line += "]]"
+            if length > length_cap:
+                line += " ..."
+            print(line)
+
+
 if __name__ == '__main__':
-    # Run time: About 6h15m (8 core parallel)
+    # Run time: ~2h40m (8-core parallel)
     read_en_article_text(chem_formula_check, process_result_callback=add_tuples_to_results, parallel=True)
-    for (formula, article_list) in articles_by_formula.items():
-        print(f"* {len(article_list)} - {formula} - [[{']], [['.join(article_list)}]]")
+    dump_results()
