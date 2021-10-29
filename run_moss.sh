2bf0eb6b6f47f36e9efa335882b31c6782a43b1c
Christopher Beland <beland@alum.mit.edu>
2021-10-29 14:21:17 -0400

    Include chemical formula report in run_moss.sh

--- run_moss.sh
+++ run_moss.sh
@@ -233,8 +233,13 @@ echo `date`
 
 echo "Starting chemical formulas report"
 echo `date`
+echo "====Possible chemical formulas that don't use subscripts====" > beland-chemical-formulas.txt
+grep -P ' ((H|He|Li|Be|B|C|N|O|F|Ne|Na|Mg|Al|Si|P|S|Cl|Ar|K|Ca|Sc|Ti|V|Cr|Mn|Fe|Co|Ni|Cu|Zn|Ga|Ge|As|Se|Br|Kr|Rb|Sr|Y|Zr|Nb|Mo|Tc|Ru|Rh|Pd|Ag|Cd|In|Sn|Sb|Te|I|Xe|Cs|Ba|La|Ce|Pr|Nd|Pm|Sm|Eu|Gd|Tb|Dy|Ho|Er|Tm|Yb|Lu|Hf|Ta|W|Re|Os|Ir|Pt|Au|Hg|Tl|Pb|Bi|Po|At|Rn|Fr|Ra|Ac|Th|Pa|U|Np|Pu|Am|Cm|Bk|Cf|Es|Fm|Md|No|Lr|Rf|Db|Sg|Bh|Hs|Mt|Ds|Rg|Cn|Nh|Fl|Mc|Lv|Ts|Og|R)([2-9]|[1-9][0-9]))+$' debug-spellcheck-ignored.txt | grep -vP ' [KQRBNP][a-h][1-8]$' | grep -vP ' [A-Z]\d\d$' | head -100 >> beland-chemical-formulas.txt
+# [KQRBNP][a-h][1-8] is to exclude [[Algebraic notation (chess)]]
 
-../venv/bin/python3 ../chemical_formula_report.py > beland-chemical-formulas.txt
+echo "" >> beland-chemical-formulas.txt
+echo "====Known chemical formulas that don't use subscripts====" >> beland-chemical-formulas.txt
+../venv/bin/python3 ../chemical_formula_report.py >> beland-chemical-formulas.txt
 
 echo "Done"
 echo `date`
