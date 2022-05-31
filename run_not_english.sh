23e249aa9414cb1bc5dd093bfde2de052fe6f3bc
Christopher Beland <beland@alum.mit.edu>
2022-05-30 22:41:58 -0400

    Include both highest # and % non-English

--- run_not_english.sh
+++ run_not_english.sh
@@ -5,10 +5,12 @@ mkdir $RUN_NAME
 cd $RUN_NAME
 
 ../venv/bin/python3 ../moss_not_english.py > not-english-output.csv
-sort not-english-output.csv -k3 -nr -t$'\t' | head -500 > tmp-not-english.csv
+# Both # and % seem like good red flags, so take the highest from both metrics
+sort not-english-output.csv -k3 -nr -t$'\t' | head -250 > tmp-not-english.csv
+sort not-english-output.csv -k2 -nr -t$'\t' | head -250 >> tmp-not-english.csv
 echo '{|class="wikitable sortable"' > post-not-english.txt
 echo '! Article !! # non-Eng !! % non-Eng !! [[Language code|Lang code]] !! Sample words' >> post-not-english.txt
-cat tmp-not-english.csv | perl -pe 's/(.*?)\t/|-\n| [[$1]]\t/' | perl -pe 's/\t/ || /g' >> post-not-english.txt
+sort not-english-output.csv -k3 -nr -t$'\t' | uniq | perl -pe 's/(.*?)\t/|-\n| [[$1]]\t/' | perl -pe 's/\t/ || /g' >> post-not-english.txt
 echo "|}" >> post-not-english.txt
 
 echo "Finished"
