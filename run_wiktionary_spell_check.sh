6746451060f66163c89049e918070bc5d62bb016
Christopher Beland <beland@alum.mit.edu>
2022-06-08 14:54:44 -0400

    Fix typo in last command of run_wiktionary_spell_check.sh

--- run_wiktionary_spell_check.sh
+++ run_wiktionary_spell_check.sh
@@ -12,7 +12,7 @@ echo `date`
 tac tmp-wiktionary-spell.txt | grep '^*' | ../venv/bin/python3 ../word_categorizer.py > tmp-words-with-articles-wikt.txt
 echo "==T1+ASCII==\n" > post-wikt-t1-ascii.txt
 grep ^T1 tmp-words-with-articles-wikt.txt | grep -P '\[\[wikt:[a-z]+\]\]' | perl -pe "s/^T1\t//" >> post-wikt-t1-ascii.txt
-grep ^HB | perl -pe "s/^HB\t//" | perl -pe 's/</&lt;/g' | perl -pe 's/>/&gt;/g' | tmp-words-with-articles-wikt.txt > post-wikt-html.txt
+grep ^HB tmp-words-with-articles-wikt.txt | perl -pe "s/^HB\t//" | perl -pe 's/</&lt;/g' | perl -pe 's/>/&gt;/g' > post-wikt-html.txt
 
 echo "Done post-processing"
 echo `date`
