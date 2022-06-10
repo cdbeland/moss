f7516f7143b5c3faff16a4f37da5c60be170c64c
Christopher Beland <beland@alum.mit.edu>
2022-06-10 14:41:32 -0400

    Fix typo in run_wiktionary_spell_check.sh

--- run_wiktionary_spell_check.sh
+++ run_wiktionary_spell_check.sh
@@ -10,7 +10,8 @@ echo "Post-processing Wiktionary results"
 echo `date`
 
 tac tmp-wiktionary-spell.txt | grep '^*' | ../venv/bin/python3 ../word_categorizer.py > tmp-words-with-articles-wikt.txt
-echo "==T1+ASCII==\n" > post-wikt-t1-ascii.txt
+echo "==T1+ASCII==" > post-wikt-t1-ascii.txt
+echo >> post-wikt-t1-ascii.txt
 grep ^T1 tmp-words-with-articles-wikt.txt | grep -P '\[\[wikt:[a-z]+\]\]' | perl -pe "s/^T1\t//" >> post-wikt-t1-ascii.txt
 grep ^HB tmp-words-with-articles-wikt.txt | perl -pe "s/^HB\t//" | perl -pe 's/</&lt;/g' | perl -pe 's/>/&gt;/g' > post-wikt-html.txt
 
