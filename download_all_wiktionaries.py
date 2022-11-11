ebbea0e560093f0ad503e29c04dbce10bf6bc7ca
Christopher Beland <beland@alum.mit.edu>
2022-11-11 14:29:55 -0500

    Faster download on recovery runs

--- download_all_wiktionaries.py
+++ download_all_wiktionaries.py
@@ -1,5 +1,6 @@
-from time import sleep
+from os.path import exists
 import re
+from time import sleep
 import urllib.request
 
 
@@ -9,10 +10,14 @@ language_codes = re.findall(r"([_a-z]+)wiktionary</a>: <span class='(done|partia
 
 for (language_code, _dump_status) in language_codes:
     file_name = "%swiktionary-latest-all-titles-in-ns0.gz" % language_code
+    full_path = "/bulk-wikipedia/all-wiktionaries/%s" % file_name
+    if exists(full_path):
+        print(f"Skipping existing file - {full_path}")
+        continue
     dump_url = "https://dumps.wikimedia.org/%swiktionary/latest/%s" % (language_code, file_name)
     sleep(1)
     print("Downloading %s" % dump_url)
     try:
-        urllib.request.urlretrieve(dump_url, "/bulk-wikipedia/all-wiktionaries/%s" % file_name)
+        urllib.request.urlretrieve(dump_url, full_path)
     except urllib.error.HTTPError:
         print("FAILED, skipping...")
