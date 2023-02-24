bf965ef74a89d22641b992d335cf1ca6cca3e29c
Christopher Beland <beland@alum.mit.edu>
2023-02-24 14:28:12 -0500

    download_moss_subpages.py: Better retry loop

--- download_moss_subpages.py
+++ download_moss_subpages.py
@@ -1,8 +1,19 @@
 import string
+from time import sleep
 import urllib.request
 
 
-for letter in ["before_A"] + list(string.ascii_uppercase) + ["after_Z"]:
+def download_page(letter, delay=10):
     url = f"https://en.wikipedia.org/wiki/Wikipedia:Typo_Team/moss/{letter}"
     print(f"Downloading {url}")
-    urllib.request.urlretrieve(url, f"/bulk-wikipedia/moss-subpages/{letter}")
+    try:
+        urllib.request.urlretrieve(url, f"/bulk-wikipedia/moss-subpages/{letter}")
+    except (urllib.error.URLError,  urllib.error.HTTPError) as error:
+        print(error)
+        print(f"Temporary failure? Retrying in {delay} seconds")
+        sleep(delay)
+        download_page(letter, delay * 2)
+
+
+for letter in ["before_A"] + list(string.ascii_uppercase) + ["after_Z"]:
+    download_page(letter)
