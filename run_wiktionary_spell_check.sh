858faafdbcb65fbc4877bfd504ca42b8959584e2
Christopher Beland <beland@alum.mit.edu>
2023-05-02 15:23:45 -0400

    Include more categories of typo for Wiktionary

--- run_wiktionary_spell_check.sh
+++ run_wiktionary_spell_check.sh
@@ -9,11 +9,46 @@ echo `date`
 echo "Post-processing Wiktionary results"
 echo `date`
 
+# Few if any English misspellings: H, BW, TF, T/, A, BC, P, L
+
 tac tmp-wiktionary-spell.txt | grep '^*' | ../venv/bin/python3 ../word_categorizer.py > tmp-words-with-articles-wikt.txt
-echo "==T1+ASCII==" > post-wikt-t1-ascii.txt
-echo >> post-wikt-t1-ascii.txt
-grep ^T1 tmp-words-with-articles-wikt.txt | grep -P '\[\[wikt:[a-z]+\]\]' | perl -pe "s/^T1\t//" >> post-wikt-t1-ascii.txt
-grep ^HB tmp-words-with-articles-wikt.txt | perl -pe "s/^HB\t//" | perl -pe 's/</&lt;/g' | perl -pe 's/>/&gt;/g' | grep -v "Unsupported titles/HTML" > post-wikt-html.txt
+echo "==Possible typos from (DUMP NAME) ==" > post-wikt-typos.txt
+
+echo "===T1+ASCII===" > post-wikt-typos.txt
+echo "Possible English-language typos." >> post-wikt-typos.txt
+echo >> post-wikt-typos.txt
+grep ^T1 tmp-words-with-articles-wikt.txt | grep -P '\[\[wikt:[a-z]+\]\]' | perl -pe "s/^T1\t//" >> post-wikt-typos.txt
+echo >> post-wikt-typos.txt
+
+echo "===ME===" >> post-wikt-typos.txt
+echo "Probable English-language compounds (with and without hyphen)." >> post-wikt-typos.txt
+echo >> post-wikt-typos.txt
+grep ^ME tmp-words-with-articles-wikt.txt | perl -pe "s/^ME\t//" >> post-wikt-typos.txt
+echo >> post-wikt-typos.txt
+
+echo "===TE===" >> post-wikt-typos.txt
+echo "Possible English-language typos." >> post-wikt-typos.txt
+echo >> post-wikt-typos.txt
+grep ^TE tmp-words-with-articles-wikt.txt | perl -pe "s/^TE\t//" >> post-wikt-typos.txt
+echo >> post-wikt-typos.txt
+
+echo "===C===" >> post-wikt-typos.txt
+echo "Chemistry-related words." >> post-wikt-typos.txt
+echo >> post-wikt-typos.txt
+grep ^C tmp-words-with-articles-wikt.txt | perl -pe "s/^C\t//" >> post-wikt-typos.txt
+echo >> post-wikt-typos.txt
+
+echo "===TS===" >> post-wikt-typos.txt
+echo "These are suspected whitespace errors." >> post-wikt-typos.txt
+echo >> post-wikt-typos.txt
+grep ^TS tmp-words-with-articles-wikt.txt | perl -pe "s/^TS\t//" >> post-wikt-typos.txt
+echo >> post-wikt-typos.txt
+
+echo "===N+DOUBLEDOT===" >> post-wikt-typos.txt
+echo >> post-wikt-typos.txt
+grep ^N tmp-words-with-articles-wikt.txt | grep "\.\." | perl -pe "s/^N\t//" >> post-wikt-typos.txt
+
+grep -P "^(HB|HL)" tmp-words-with-articles-wikt.txt | perl -pe "s/^HB\t//" | perl -pe 's/</&lt;/g' | perl -pe 's/>/&gt;/g' | grep -v "Unsupported titles/HTML" > post-wikt-html.txt
 
 echo "Done post-processing"
 echo `date`
