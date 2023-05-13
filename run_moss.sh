77de75d806ba426f8f10507abb5beda6201c6fc6
Christopher Beland <beland@alum.mit.edu>
2023-05-13 02:33:31 -0400

    More rhyme report improvements

--- run_moss.sh
+++ run_moss.sh
@@ -422,9 +422,9 @@ echo `date`
 
 ../venv/bin/python3 ../dump_grep_regex.py "[Rr]hym|[Pp]oem|[Ss]tanza|[Vv]erse|[Ll]yric" > tmp-rhyme-dump.xml
 
-cat tmp-rhyme-dump.xml | ../venv/bin/python3 ../dump_grep_inline.py "[^a-z0-9\-A-Z][Aa]-[Bb][^a-zA-Z]" | grep -v "<math" | grep -v "A-B-C_list/" | sort > tmp-rhyme-a-b.txt
+cat tmp-rhyme-dump.xml | ../venv/bin/python3 ../dump_grep_inline.py "[^a-z0-9\-A-Zí][Aa]-[Bb][^a-zA-Z]" | perl -pe '<math.*?(</math>|<\/nowiki>/ig)' | perl -pe '<ref.*?(/ ?>|</ref>|<\/nowiki>/ig)' | perl -pe 's/\{\{cite.*?(\}\}|<\/nowiki>)//ig' | grep -i "a-b" | grep -v "A-B-C-D-E-F-G" | sort > tmp-rhyme-a-b.txt
 cat tmp-rhyme-dump.xml | ../venv/bin/python3 ../dump_grep_inline.py "[^,]AB,[ABC]|AA,AB|AA,B|AB,[ABC]" | grep -v "<math" | grep -vP "^(Rhyme scheme:|The Raven:)" | sort > tmp-rhyme-AB-comma.txt
-cat tmp-rhyme-dump.xml | ../venv/bin/python3 ../dump_grep_inline.py "[^A-Za-z0-9\./%#=_\-](aa|ab|aaa|aab|aba|abb|abc|aaaa|aaba|aabb|aabc|abaa|abab|abba|abca|abcb|abcc|abcd)[^a-z0-9/]"  | sort> tmp-rhyme-masked-words.txt
+cat tmp-rhyme-dump.xml | ../venv/bin/python3 ../dump_grep_inline.py "[^A-Za-z0-9\./%#=_\-](aa|ab|aaa|aab|aba|abb|abc|aaaa|aaba|aabb|aabc|abaa|abab|abba|abca|abcb|abcc|abcd)[^a-z0-9/]" | perl -pe '<ref.*?(/ ?>|</ref>|<\/nowiki>/ig)' | grep -P "(aa|ab)"  | sort > tmp-rhyme-masked-words.txt
 
 cat tmp-rhyme-dump.xml | ../venv/bin/python3 ../dump_grep_inline.py "([^a-z\+/]a\.b\.[^d-z]|[^a-z\+/]a\. b\. [^d-z])" | grep -v "Exemplum:"  | sort > tmp-rhyme-a.b.txt
 
