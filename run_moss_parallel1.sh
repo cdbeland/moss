18a4e276f7601280fd443ef68a0586c6712b941a
Christopher Beland <beland@alum.mit.edu>
2024-01-23 17:56:22 -0500

    Suppress reports that currently have no content

--- run_moss_parallel1.sh
+++ run_moss_parallel1.sh
@@ -11,20 +11,20 @@ echo "Finished style check by line; processing..."
 echo `date`
 
 set +e
-grep ^L tmp-style-by-line.txt > fixme-liters.txt
-grep ^X tmp-style-by-line.txt > fixme-x-to-times.txt
-grep ^T tmp-style-by-line.txt > fixme-temp.txt
-grep ^S tmp-style-by-line.txt > fixme-speed.txt
-grep ^FU tmp-style-by-line.txt > fixme-fuel-efficiency.txt
-grep ^P tmp-style-by-line.txt > fixme-prime.txt
-grep ^FR tmp-style-by-line.txt > fixme-fraction.txt
 grep ^N tmp-style-by-line.txt > fixme-broken-nbsp.txt
 grep ^R tmp-style-by-line.txt > fixme-rhyme-scheme.txt
-
+grep ^X tmp-style-by-line.txt > fixme-x-to-times.txt
+grep ^FR tmp-style-by-line.txt > fixme-fraction.txt
 grep ^QL tmp-style-by-line.txt > fixme-logical-quoting.txt
-grep ^QB tmp-style-by-line.txt > fixme-bad-quoting.txt
 
-grep ^QD tmp-style-by-line.txt | perl ../count.pl | sort -rn | perl -pe "s/^\d+\t//" | head -1000 > jwb-double-most.txt
+echo "moss_check-style_by_line.py needs rewrite for performance" > INCOMPLETE.txt
+# grep ^L tmp-style-by-line.txt > fixme-liters.txt
+# grep ^T tmp-style-by-line.txt > fixme-temp.txt
+# grep ^S tmp-style-by-line.txt > fixme-speed.txt
+# grep ^FU tmp-style-by-line.txt > fixme-fuel-efficiency.txt
+# grep ^P tmp-style-by-line.txt > fixme-prime.txt
+# grep ^QB tmp-style-by-line.txt > fixme-bad-quoting.txt
+# grep ^QD tmp-style-by-line.txt | perl ../count.pl | sort -rn | perl -pe "s/^\d+\t//" | head -1000 > jwb-double-most.txt
 set -e
 
 # --- READABILITY ---
