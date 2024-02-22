7ab1744a3a05cc017da4ba1d1b3cce4641fa4a38
Christopher Beland <beland@alum.mit.edu>
2024-02-22 16:22:17 -0500

    rm -f tmp-retf.csv

--- run_moss_parallel1.sh
+++ run_moss_parallel1.sh
@@ -46,6 +46,7 @@ echo `date`
 cat tmp-retf.csv | perl -pe 's/^.*\t(.*?)\t.*?$/$1/' | sort | perl ../count.pl | sort -n > retf-counts.txt
 sort tmp-retf.csv --stable -n -k5 -t "	" > retf-by-regex.csv
 # Above, -t "(tab character)"
+rm -f tmp-retf.csv
 cat retf-counts.txt | perl -pe 's/^(.*?)\t(.*?)$/    "$2": $1,/' > retf-stat-array.py
 cat retf-by-regex.csv | grep -vP "(0–0–0|0–0|2–1–1|2–1|ELLIPSIS|'s)" | perl -pe 's/^(.*?)\t.*$/$1/' | uniq | head -5000 > retf-high-priority-articles.txt
 
