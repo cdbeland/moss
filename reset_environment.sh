7b842fc946947e6fb53a33823a98df6219c61aa6
Christopher Beland <beland@alum.mit.edu>
2025-07-14 20:52:02 -0400

    Add missing punkt_tab for NLTK (probably due to Ubuntu OS upgrade)

--- reset_environment.sh
+++ reset_environment.sh
@@ -8,4 +8,5 @@ venv/bin/pip install --upgrade pip
 venv/bin/pip install wheel
 venv/bin/pip install -r requirements.txt
 venv/bin/python -c "import nltk; nltk.download('punkt')"
+venv/bin/python -c "import nltk; nltk.download('punkt_tab')"
 venv/bin/python -c "import nltk; nltk.download('averaged_perceptron_tagger')"
