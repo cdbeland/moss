c9f45245980a949202bcaac11cc70af8ef32eac3
Christopher Beland <beland@alum.mit.edu>
2025-08-04 16:13:13 -0400

    Update run times and note why this script is disabled

--- update_downloads_parallel3.sh
+++ update_downloads_parallel3.sh
@@ -1,31 +1,43 @@
 #!/usr/bin/bash
 set -e
-# Loading enwiki categories
+
+# DISABLED DUE TO EXCESSIVE TIME TO COMPLETION. USING API QUERIES
+# INSTEAD BECAUSE ONLY A SMALL NUMBER OF CATEGORIES ARE NEEDED. IF
+# THIS CHANGES IN THE FUTURE, IT'S POSSIBLE THIS SQL LOADING TECHNIQUE
+# IS SUBOPTIMAL?
+
+# Loads enwiki categories
 
 cd /var/local/moss/bulk-wikipedia/
 
 echo `date`
+
+# Run time: About 20 min
 echo "Adding enwiki categories..."
 echo "Starting downloads and decompression..."
-
 rm -f enwiki-latest-categorylinks.sql
 wget -o - --no-verbose https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-categorylinks.sql.gz
 gunzip enwiki-latest-categorylinks.sql.gz
+
 echo `date`
+
+# Run time: ~52 hours!!!
 echo "Loading enwiki-latest-categorylinks.sql..."
 cat enwiki-latest-categorylinks.sql | mysql -D enwiki
 
+# Fast
 rm -f enwiki-latest-page.sql
 wget -o - --no-verbose https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-page.sql.gz
 gunzip enwiki-latest-page.sql
 
 echo `date`
+# Run time: ~2 hours
 echo "Loading enwiki-latest-page.sql..."
 cat enwiki-latest-page.sql | mysql -D enwiki
 
 echo `date`
 echo "Building page_categories table..."
-# Run time: About 25 hours
+# Run time: About 3 hours
 
 echo "DROP TABLE IF EXISTS page_categories;" | mysql -D enwiki
 echo "CREATE TABLE page_categories (
