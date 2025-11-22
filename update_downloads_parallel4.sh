e643de94af398234f96385e4cde9df1e51dd38e1
Christopher Beland <beland@alum.mit.edu>
2025-11-21 22:21:38 -0500

    Produce list of redirects at download time

--- update_downloads_parallel4.sh
+++ update_downloads_parallel4.sh
@@ -85,7 +85,7 @@ venv/bin/python3 transliterate.py > /var/local/moss/bulk-wikipedia/transliterati
 echo `date`
 echo "Decompressing enwiktionary multistream and converting XML to CSV..."
 # Run time: About 2 hours
-bunzip2 -c /var/local/moss/bulk-wikipedia/enwiktionary-latest-pages-articles-multistream.xml.bz2 | venv/bin/python3 xml_to_csv.py > /var/local/moss/bulk-wikipedia/enwiktionary-articles-no-redir.csv
+bunzip2 -c /var/local/moss/bulk-wikipedia/enwiktionary-latest-pages-articles-multistream.xml.bz2 | venv/bin/python3 xml_to_csv.py enwiktionary
 rm -f /var/local/moss/bulk-wikipedia/enwiktionary-latest-pages-articles-multistream.xml.bz2
 
 echo `date`
--- xml_to_csv.py
+++ xml_to_csv.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 
-# http://dumps.wikimedia.org/backup-index.html
-# http://meta.wikimedia.org/wiki/Data_dumps
+# Usage:
+#  cat enwiki-latest-pages-articles-multistream.xml | venv/bin/python3 xml_to_csv.py enwiki
+#
+# This script will create 2 CSV files (one with articles and one with
+# redirects) named starting with the specified base name.
+
+# Files come from:
+#  http://dumps.wikimedia.org/backup-index.html
+# XML format info:
+#  http://meta.wikimedia.org/wiki/Data_dumps
 
 import lxml.etree
 import re
@@ -11,7 +19,15 @@ PAGE_RE = re.compile(r"^.*(<page.*?</page>).*$", flags=re.MULTILINE+re.DOTALL)
 NEWLINE_RE = re.compile(r"\n")
 
 if __name__ == '__main__':
+    base_name = sys.argv[1]
+    if not base_name:
+        print("NO BASE NAME SPECIFIED")
+        exit(1)
+
     working_string = ""
+    article_file = open(f"{base_name}-articles-no-redir.csv", "w")
+    redirect_file = open(f"{base_name}-redirects.csv", "w")
+
     for line in sys.stdin:
         working_string += line
         if line == "  </page>\n":
@@ -24,13 +40,15 @@ if __name__ == '__main__':
                 # Page is not an article
                 continue
 
-            redirect_target = root_element.find('redirect')
-            if redirect_target is not None:
-                # Article is redirect; skip it for now
-                continue
-
             article_title = root_element.findtext('title')
-            article_text = root_element.findtext('.//text')
-            sys.stdout.write(f"{article_title}\t{article_text}\r")
-            # Using linefeed as line separator to avoid
-            # conflicting with newlines in article_text
+            redirect_element = root_element.find('redirect')
+            if redirect_element is not None:
+                redirect_target = redirect_element.get("title")
+                if not redirect_target:
+                    raise Exception(f"{article_title}: missing redirect target")
+                print(f"{article_title}\t{redirect_target}", file=redirect_file)
+            else:
+                article_text = root_element.findtext('.//text')
+                article_file.write(f"{article_title}\t{article_text}\r")
+                # Using linefeed as line separator to avoid
+                # conflicting with newlines in article_text
