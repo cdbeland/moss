5ccaab2adf6135611264c2bbe88849f74a82c8f0
Christopher Beland <beland@alum.mit.edu>
2025-12-19 05:03:49 -0500

    Further digestion of broke-nbsp report for convenience

--- run_moss_parallel1.sh
+++ run_moss_parallel1.sh
@@ -12,6 +12,7 @@ echo `date`
 
 set +e
 grep ^N tmp-style-by-line.txt | sort > fixme-broken-nbsp.txt
+cat fixme-broken-nbsp.txt | perl -pe 's/N\t(.*?)\t.*$/$1/' | uniq | sort > fixme-broken-nbsp-articles.txt
 grep ^R tmp-style-by-line.txt | sort | perl -pe 's/^R\t(.*?)\t(.*)/* [[$1]] - <nowiki>$2<\/nowiki>/' > fixme-rhyme-scheme.txt
 grep ^X tmp-style-by-line.txt | sort > fixme-x-to-times.txt
 grep ^FR tmp-style-by-line.txt | sort > fixme-fraction.txt
