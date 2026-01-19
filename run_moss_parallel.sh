092799d325ec7dcb96e29669e633b5fa4ac87a07
Christopher Beland <beland@alum.mit.edu>
2026-01-19 13:23:23 -0500

    Run serially to reduce RAM requirements

--- run_moss_parallel.sh
+++ run_moss_parallel.sh
@@ -38,23 +38,23 @@ echo `date`
 ../venv/bin/python3 ../moss_entity_check.py > tmp-entities
 cat tmp-entities | ../venv/bin/python3 ../summarizer.py --find-all > post-entities.txt
 
+export LC_ALL=C
+grep '*' tmp-entities | perl -pe 's/\]\], \[\[/\n/g' | perl -pe 's/.*\[\[//' | perl -pe 's/\]\]//'  | sort | uniq > jwb-entities-alpha.txt
 
 # --- PARALLELIZED REPORTS ---
 
-# Run multiple main threads because even though most calculations are
-# parallelized to use all cores, some are not, and sometimes the
-# parent thread becomes a bottleneck, and CPUs are underused. Only run
-# 2 tasks at a time to avoid CPU bottleneck that slows down the first
-# reports to finish (which are usually urgently needed).
+# Run serially to avoid exceeding 8 MB RAM
+../run_moss_parallel1.sh >& thread2.log
+../run_moss_parallel2.sh >& thread1.log
 
-../run_moss_parallel1.sh >& thread1.log &
-../run_moss_parallel2.sh >& thread2.log &
-
-echo "Parallel threads started."
-echo `date`
-
-export LC_ALL=C
-grep '*' tmp-entities | perl -pe 's/\]\], \[\[/\n/g' | perl -pe 's/.*\[\[//' | perl -pe 's/\]\]//'  | sort | uniq > jwb-entities-alpha.txt
+# Sometimes there can be a CPU bottleneck for parent threads, leaving
+# child threads underused, so starting two scripts at the same time
+# can be more efficient. Also, not all long calculations can be
+# parallelized to use all cores.
+# ../run_moss_parallel1.sh >& thread1.log &
+# ../run_moss_parallel2.sh >& thread2.log &
+# echo "Parallel threads started."
+# echo `date`
 
 echo "Main thread done."
 echo `date`
